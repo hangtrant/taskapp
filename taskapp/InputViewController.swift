7891481f21035778b89a5c196ae690750b7c37e6
thuhang.tran <trant@cocoto.co.jp>
2021-09-06 12:54:16 +0900

    pod install と .gitignoreの作成まで実施

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,22 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by チャン　トゥハン on 2021/09/06.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
